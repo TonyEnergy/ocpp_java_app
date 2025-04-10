7f229c34db2ff60bf35e4fd52a63d67127b756e1
TonyEnergy <1933255124@qq.com>
2025-04-10 14:56:53 +0800

    Add logback support

--- src/main/java/github/tonyenergy/controller/HealthController.java
+++ src/main/java/github/tonyenergy/controller/HealthController.java
@@ -1,8 +1,11 @@
 package github.tonyenergy.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * @Author: liutaohua
  * @Date: 2025/4/9
@@ -12,7 +15,7 @@ public class HealthController {
 
     @GetMapping("/ping")
     public String ping() {
-        System.out.println("Server received ping command");
+        logger.log("📩 Server Received message：ping");
         return "pong";
     }
 }

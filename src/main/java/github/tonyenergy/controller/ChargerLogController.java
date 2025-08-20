a5a55de0f7598ef8869b5287b87eadd59e8e11cc
TonyEnergy <1933255124@qq.com>
2025-08-20 15:35:56 +0800

    Bugfix: change api path, conflict with charger controller

--- src/main/java/github/tonyenergy/controller/ChargerLogController.java
+++ src/main/java/github/tonyenergy/controller/ChargerLogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api")
+@RestController("/chargerLog")
 @Slf4j
 public class ChargerLogController {
 
